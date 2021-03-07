c1c9080e63c75e8447c2a643701b997686884f8a
k.naveen kumar <naveenkod22@users.noreply.github.com>
2021-03-07 18:47:13 +0530

    initial commit

---
+++ Query/12.10 1NF first normal form.sql
@@ -0,0 +1,4 @@
+-- 12.10 1NF first normal form
+-- each cell should have a single value and we can't have repeted columns
+
+-- course.tags column vilotes this rule, so we create seperate table for tags
---
+++ Query/12.9 Normalization.sql
@@ -0,0 +1,7 @@
+-- 12.9 Normalization
+-- redundency(duplicate data) is bad, more places to update, may lead to in consist data, size of database increases
+
+-- normalization is the process of reviewing design that folows pre defined rules, that avoids data duplication
+-- ther are 7 rules, each rule assumes you apply previous rule
+
+-- most of time first 3 rules are enough
